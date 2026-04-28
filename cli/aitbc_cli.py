e520622ff11d3abc73e913129345f03c0f58c7d3
aitbc <aitbc@keisanki.net>
2026-04-28 15:53:58 +0200

    fix: add type field to CLI transaction construction

--- cli/aitbc_cli.py
+++ cli/aitbc_cli.py
@@ -232,6 +232,7 @@ def send_transaction(from_wallet: str, to_address: str, amount: float, fee: floa
     
     # Create transaction
     transaction = {
+        "type": "TRANSFER",
         "chain_id": chain_id,
         "from": sender_address,
         "to": to_address,
