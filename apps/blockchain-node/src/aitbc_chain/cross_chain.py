a2e2aedeee168a2cee0d8f380276d19e90490a72
aitbc <aitbc@keisanki.net>
2026-05-02 12:29:15 +0200

    Fix UTC usage in cross-chain sync helpers

--- apps/blockchain-node/src/aitbc_chain/cross_chain.py
+++ apps/blockchain-node/src/aitbc_chain/cross_chain.py
@@ -18,7 +18,7 @@ class CrossChainSync:
             self.sync_status[chain] = {
                 "synced": True,
                 "height": 0,
-                "last_sync": datetime.now(datetime.UTC).isoformat(),
+                "last_sync": datetime.now(UTC).isoformat(),
             }
 
 
@@ -36,5 +36,5 @@ class MultiChainConsensus:
                 "consensus_reached": True,
                 "height": 0,
                 "validators": 1,
-                "last_consensus": datetime.now(datetime.UTC).isoformat(),
+                "last_consensus": datetime.now(UTC).isoformat(),
             }
